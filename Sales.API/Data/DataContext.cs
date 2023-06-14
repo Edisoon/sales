69fd68cc8448012c6b5ad30d08bf51a5ca3a2308
Edisoon <Edison_theM3ka@hotmail.com>
2023-06-13 23:29:28 -0400

    Add data context, conection sstring and create database

---
+++ Sales.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {   
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+            //Metodos para crear indices unicos en las entidades
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
